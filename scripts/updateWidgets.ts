c4babdbbad6aa899269b4d2a8d08d1965db50b04
ik1s3v <ik1s3v@gmail.com>
2026-04-13 14:47:00 +0300

    init

---
+++ scripts/updateWidgets.ts
@@ -0,0 +1,32 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+import * as core from "@actions/core";
+import type { IManifest } from "@widy/sdk";
+
+try {
+	const widgetPath = String(process.env.WIDGET_PATH);
+
+	const id = String(process.env.WIDGET_ID);
+
+	const manifestPath = join(widgetPath, "manifest.json");
+
+	const manifest = JSON.parse(readFileSync(manifestPath, "utf-8")) as IManifest;
+
+	const widgetsPath = join(widgetPath, "widgets.json");
+
+	let widgets: Record<string, IManifest>;
+	try {
+		widgets = JSON.parse(readFileSync(widgetsPath, "utf-8"));
+	} catch {
+		widgets = {};
+	}
+
+	widgets[id] = manifest;
+
+	writeFileSync(widgetsPath, JSON.stringify(widgets, null, 2), {
+		encoding: "utf-8",
+	});
+} catch (error) {
+	const msg = error instanceof Error ? error.message : String(error);
+	core.setFailed(msg);
+}
